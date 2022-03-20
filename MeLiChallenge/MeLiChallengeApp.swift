7652aa40d4d6043dfcdaae2076fadbfb0da06b69
Orlando Pulido <orlando.pulido@pragma.com.co>
2022-03-20 11:18:18 -0500

    Initial Commit

---
+++ MeLiChallenge/MeLiChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeLiChallengeApp.swift
+//  MeLiChallenge
+//
+//  Created by Orlando Pulido Marenco on 20/03/22.
+//
+
+import SwiftUI
+
+@main
+struct MeLiChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
